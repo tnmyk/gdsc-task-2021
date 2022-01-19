c3406e22aa93817f17f3930babaa8f0a8b232783
Tanmaykk <tanmaykachroo@gmail.com>
2022-01-19 14:20:49 +0530

    Added home card card animation

--- src/components/Pages/Home/Card.js
+++ src/components/Pages/Home/Card.js
@@ -5,7 +5,7 @@ const Card = ({ data }) => {
     <Link
       to={data.link}
       style={{ backgroundColor: "#3C3C3C" }}
-      className="w-10/12  sm:w-1/3 rounded-xl p-4 pt-7 pb-5 flex flex-col items-center"
+      className="home-card w-10/12 sm:w-1/3 rounded-xl p-4 pt-7 pb-5 flex flex-col items-center"
     >
       <img src={data.image} alt="nmo" width="140px" />
       <h1 style={{ color: "#ECECEC" }} className="text-2xl font-medium mt-2">
